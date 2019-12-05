fee2dcc226cd4c6eea1da439da656761ae18256f
Hubner Krisztian <krisztian.hubner@tappointment.com>
2019-12-05 18:27:02 +0100

    Fix IProductService's createProduct() to use specific input type

--- src/services/IProductService.ts
+++ src/services/IProductService.ts
@@ -1,10 +1,10 @@
 import { Product } from "../models/Product";
-import { ProductDTO } from "../dto/ProductDTO";
+import { CreateProductInput } from "../dto/ProductDTO";
 
 export interface IProductService {
   getProductsByIds(ids: string[]): Promise<Array<Product | null>>;
   getAllProducts(): Promise<Product[]>;
-  createProduct(data: ProductDTO): Promise<Product>;
+  createProduct(data: CreateProductInput): Promise<Product>;
   getProductById(productId: string): Promise<Product>;
   search(text: string): Promise<Product[]>;
 }
