091a8e7e9d5602a835d18c945128b95fa5b58a06
Hubner Krisztian <krisztian.hubner@tappointment.com>
2019-12-05 18:29:28 +0100

    Add amountType to ProductDTO

--- src/dto/ProductDTO.ts
+++ src/dto/ProductDTO.ts
@@ -1,15 +1,18 @@
 import * as yup from "yup";
+import { AmountType } from "../enums";
 
 export interface ProductDTO {
   name: string;
   description?: string;
   estimatedPrice: number;
+  amountType: AmountType;
 }
 
 export const createProductValidator = yup.object({
   name: yup.string().required(),
   description: yup.string().ensure(),
   estimatedPrice: yup.number().positive(),
+  amountType: yup.mixed().oneOf(Object.values(AmountType)).required(),
 });
 
 export type CreateProductInput = yup.InferType<typeof createProductValidator>;
