ff72f8b7c5dd5d6adad63c6ac1cd7208fe74167c
Hubner Krisztian <krisztian.hubner@tappointment.com>
2019-12-12 11:51:17 +0100

    Add types to process env

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,7 +1,11 @@
-declare namespace NodeJS
-{
-  export interface ProcessEnv
-  {
+declare namespace NodeJS {
+  export interface ProcessEnv {
     JWT_SECRET: string;
+    SEED_ADMIN_EMAIL: string;
+    SEED_ADMIN_PASSWORD: string;
+    SEED_COURIER_EMAIL: string;
+    SEED_COURIER_PASSWORD: string;
+    SEED_NORMAL_EMAIL: string;
+    SEED_NORMAL_PASSWORD: string;
   }
 }
