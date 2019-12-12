7edadeadd63b169833ccd04e3eff3f274a11e1e4
Hubner Krisztian <krisztian.hubner@tappointment.com>
2019-12-12 11:50:21 +0100

    Change input to the corresponding input type

--- src/services/IUserService.ts
+++ src/services/IUserService.ts
@@ -1,11 +1,11 @@
 import { User } from "../models/User";
-import { UserDTO } from "../dto/UserDTO";
+import { CreateUserInput } from "../dto/UserDTO";
 
 export interface IUserService {
   getUserById(id: string): Promise<User>;
   getUsersByIds(ids: string[]): Promise<User[]>;
   getAllUsers(): Promise<User[]>;
-  createUser(input: UserDTO): Promise<User>;
+  createUser(input: CreateUserInput): Promise<User>;
   applyForCourier(userId: string): Promise<User>;
-  uploadMoney(options: { userId: string, amount: number }): Promise<User>;
+  uploadMoney(options: { userId: string; amount: number }): Promise<User>;
 }
